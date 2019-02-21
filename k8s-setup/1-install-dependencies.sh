48bbcef183fcb3a2c134661a09343b8d24a4094f
Pawel Zalejko <p.zalejko@gmail.com>
2019-02-21 23:05:55 +0100

    updated k8s setup

--- k8s-setup/1-install-dependencies.sh
+++ k8s-setup/1-install-dependencies.sh
@@ -22,6 +22,8 @@ add-apt-repository \
    stable"
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+rm /etc/apt/sources.list.d/kubernetes.list
+touch /etc/apt/sources.list.d/kubernetes.list
 cat <<EOT >> /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOT
