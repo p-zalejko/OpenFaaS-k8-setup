bda31369261b41ae744db6681eec34430df40a0f
Pawel Zalejko <p.zalejko@gmail.com>
2019-02-22 21:30:27 +0100

    updated k8s setup

--- k8s-setup/2-install-k8s.sh
+++ k8s-setup/2-install-k8s.sh
@@ -10,13 +10,13 @@ kubeadm config images pull
 kubeadm init
 
 mkdir -p $USER_HOME/.kube
-cp -i /etc/kubernetes/admin.conf $USER_HOME/.kube/config
-chown -R $USER_NAME:$USER_NAME $USER_HOME/.kube
-
 sysctl net.bridge.bridge-nf-call-iptables=1
 
-sleep 30
 echo "waiting for k8s setup..."
+sleep 30
+
+cp -i /etc/kubernetes/admin.conf $USER_HOME/.kube/config
+chown -R $USER_NAME:$USER_NAME $USER_HOME/.kube
 
 # Install Weave Net as overlay network
 export kubever=$(kubectl version | base64 | tr -d '\n')
@@ -24,4 +24,3 @@ kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
 
 # Taint the master node so application pods can run on it too
 kubectl taint nodes --all node-role.kubernetes.io/master-
-
--- k8s-setup/99-uninstall-k8s.sh
+++ k8s-setup/99-uninstall-k8s.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo kubeadm reset
-sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
-sudo apt-get autoremove
-sudo rm -rf ~/.kube
+kubeadm reset
+apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
+apt-get autoremove
+rm -rf ~/.kube
 
 # Delete all containers
 docker rm $(docker ps -a -q)
 # Delete all images
 docker rmi $(docker images -q)
-sudo reboot now
+reboot now
