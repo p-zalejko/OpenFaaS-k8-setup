ba8ab79fc11f5c96603f84ab687e45fff617a939
Pawel Zalejko <p.zalejko@gmail.com>
2019-02-22 21:51:35 +0100

    updated k8s setup

--- k8s-setup/3-install-k8s-network.sh
+++ k8s-setup/3-install-k8s-network.sh
@@ -3,6 +3,3 @@
 # Install Weave Net as overlay network
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
-
-# Taint the master node so application pods can run on it too
-kubectl taint nodes --all node-role.kubernetes.io/master-
