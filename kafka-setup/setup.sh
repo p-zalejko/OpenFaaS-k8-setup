d1f8af7ab183c957467fc0471acf14fe1267b25b
Pawel Zalejko <p.zalejko@gmail.com>
2019-02-24 21:56:50 +0100

    updated kafka setup

--- kafka-setup/setup.sh
+++ kafka-setup/setup.sh
@@ -3,6 +3,13 @@
 kubectl create namespace kafka 
 
 kubectl apply -f zoo.yaml -n kafka
+
+# let's wait for zookeeper
+sleep 45
+
 kubectl apply -f kafka-micro.yaml -n kafka
-kubectl apply -f kafka-rest-proxy.yaml -n kafka
+
+# let's wait for kafka
+sleep 60
 kubectl apply -f kafka-manager.yml -n kafka
+kubectl apply -f kafka-rest-proxy.yaml -n kafka
