ef941690c758eb766e103f516475c95c6ee663b9
Pawel Zalejko <p.zalejko@gmail.com>
2019-02-24 21:54:37 +0100

    updated openfaas setup

--- faas-setup/setup.sh
+++ faas-setup/setup.sh
@@ -2,29 +2,20 @@
 
 # see
 # https://medium.com/devopslinks/getting-started-with-openfaas-on-minikube-634502c7acdf
-#
+# https://blog.alexellis.io/serverless-kubernetes-on-raspberry-pi/
+
+#!/bin/sh
 
 # faas CLI
 curl -sL cli.openfaas.com | sudo sh
 
-kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
-helm repo add openfaas https://openfaas.github.io/faas-netes/
-helm repo update
+git clone https://github.com/openfaas/faas-netes
 
-sleep 10
+kubectl apply -f ./faas-netes/namespaces.yml
+kubectl apply -f ./faas-netes/yaml_arm64
+# expose faas dashboard via traefik
+kubectl apply -f openfaaas-ingress.yaml
 
-export PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
-echo $PASSWORD
-
-kubectl -n openfaas create secret generic basic-auth --from-literal=basic-auth-user=admin --from-literal=basic-auth-password="$PASSWORD"
-helm upgrade openfaas --install openfaas/openfaas --namespace openfaas --set functionNamespace=openfaas-fn --set basic_auth=true
-
-#export MINIKUBE_IP=192.168.99.100
-export MINIKUBE_IP=$(minikube ip)
-export OPENFAAS_URL=$MINIKUBE_IP:31112
-echo $OPENFAAS_URL
-
-#echo -n $PASSWORD | faas-cli login -g http://$OPENFAAS_URL -u admin — password-stdin
-faas-cli login -u admin -p $PASSWORD -g http://$OPENFAAS_URL
-
-
+# from an external pc:
+export OPENFAAS_URL=http://openfaas-gateway.internal.dom.com
+faas-cli list
